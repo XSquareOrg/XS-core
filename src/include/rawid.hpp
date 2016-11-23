4d03ed35eef779b376edd3187155a7c69f6e2b7d
Speks of Dust <speksofdust@users.noreply.github.com>
2016-11-22 21:00:35 -0500

    added include/ rawid.hpp, refcount.hpp, vc.hpp

---
+++ src/include/rawid.hpp
@@ -0,0 +1,81 @@
+/* ------------------------------------------------------------------------ *
+ *   XSquare is free software: you can redistribute it and/or               *
+ *   modify it under the terms of the GNU Lesser General Public License as  *
+ *   published by the Free Software Foundation version 3 of the Licence, or *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   XSquare is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   General Public License for more details.                               *
+ *                                                                          *
+ *   You should have received copies of the GNU General Public License and  *
+ *   the GNU Lesser General Public License along with this program.  If     *
+ *   not, see http://www.gnu.org/licenses/.                                 *
+* ------------------------------------------------------------------------- */
+
+
+#ifndef XS_CORE_RAWID__
+#define XS_CORE_RAWID__
+
+namespace XS_CORE {
+
+class RawID {
+protected:
+    unsigned rid = 0;
+public:
+    explicit RawID() {}
+    explicit RawID(unsigned v) {this->rid = v;}
+    virtual ~RawID();
+    const unsigned &ID(void) {return this->rid;}
+    operator const unsigned() {return this->rid;}
+    operator unsigned() {return this->rid;}
+    bool operator==(const RawID &rhs) {return this->rid == rhs.rid;}
+    bool operator==(const unsigned &rhs) {return this->rid == rhs;}
+
+    bool operator!=(const RawID &rhs) {return !this->operator==(rhs);}
+    bool operator!=(const unsigned &rhs) {return !this->operator==(rhs);}
+
+    bool operator<(const RawID &rhs) {return this->rid < rhs.rid;}
+    bool operator<(const unsigned &rhs) {return this->rid < rhs;}
+
+    bool operator>(const RawID &rhs) {return this->rid > rhs.rid;}
+    bool operator>(const unsigned &rhs) {return this->rid > rhs;}
+
+    bool operator<=(const RawID &rhs) {return this->rid <= rhs.rid;}
+    bool operator<=(const unsigned &rhs) {return this->rid <= rhs;}
+
+    bool operator>=(const RawID &rhs) {return this->rid >= rhs.rid;}
+    bool operator>=(const unsigned &rhs) {return this->rid >= rhs;}
+};
+
+
+class MgrRawID: public RawID {
+protected:
+    unsigned _last = 0;
+    bool _locked = false;
+public:
+    friend class RawID;
+    MgrRawID() {
+        this->rid++;
+    }
+    ~MgrRawID() {}
+    bool operator==(const MgrRawID &rhs) {return this->rid == rhs.rid;}
+    bool operator!=(const MgrRawID &rhs) {return !this->operator==(rhs);}
+    RawID new_RawID(void) {
+        while (1) {
+            if (!this->_locked) {
+                this->_locked = true;
+                RawID r;
+                r = RawID(this->_last);
+                this->_last++;
+                this->_locked = false;
+                return r;
+            }
+        }
+
+    }
+};
+
+} // XS_CORE
+#endif // XS_CORE_RAWID__
---
+++ src/include/vc.hpp
@@ -0,0 +1,60 @@
+/* ------------------------------------------------------------------------ *
+ *   XSquare is free software: you can redistribute it and/or               *
+ *   modify it under the terms of the GNU Lesser General Public License as  *
+ *   published by the Free Software Foundation version 3 of the Licence, or *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   XSquare is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   General Public License for more details.                               *
+ *                                                                          *
+ *   You should have received copies of the GNU General Public License and  *
+ *   the GNU Lesser General Public License along with this program.  If     *
+ *   not, see http://www.gnu.org/licenses/.                                 *
+* ------------------------------------------------------------------------- */
+
+
+#ifndef XS_CORE_VC__
+#define XS_CORE_VC__
+
+#include <iostream>
+#include <vector>
+
+namespace XS_CORE {
+
+template <class ITEM_T, class CLS_T>
+class VC { // Vector container class
+protected:
+    virtual ~VC();
+    std::vector<ITEM_T> items;
+    void _del_item(ITEM_T item) {this._del_item_by_idx(this->indexof(item));}
+    void _del_item_by_idx(const unsigned i) {
+        if (i == 0 || i <= this->size())
+            this->items.erase(this->items.begin()+i);
+    }
+    void _add_item(ITEM_T &item) {this->items.push_back;}
+public:
+    inline unsigned size(void) const {return this->items.size();}
+    inline bool is_empty(void) {return this->items.size() == 0;}
+    size_t indexof(ITEM_T item) {
+        // returns -1 if item does not exist
+        const unsigned s = this->items.size();
+        for (unsigned i=0; i<s; i++)
+            if (this->items[i] == item) return i;
+        return -1;
+    }
+    bool has_item(ITEM_T item) {
+        const unsigned s = this->items.size();
+        for (unsigned i=0; i<s; i++)
+            if (this->items[i] == item) return true;
+        return false;
+    }
+    bool operator==(const CLS_T &rhs) {return this->items->operator==(rhs);}
+    bool operator!=(const CLS_T &rhs) {return !this->operator==(rhs);}
+    ITEM_T& operator[](size_t i) {return this->items[i];}
+    const ITEM_T& operator[](size_t i) const {return this->items[i];}
+};
+
+} // XS_CORE
+#endif // XS_CORE_VC__
