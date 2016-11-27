e27e3ed9ca69e45a93555cbcb66a92971493180c
Speks of Dust <speksofdust@users.noreply.github.com>
2016-11-26 23:23:39 -0500

    completed funcs for mmi_2

--- src/simd/mm2.hpp
+++ src/simd/mm2.hpp
@@ -25,23 +25,24 @@ namespace xs_core {
 
 template <class U, class T>
 class _mm2: public mm_basemaths_T<U, T> {
+    struct {U x, y;} data;
 public:
     inline const unsigned size(void) {return 2;}
     inline void zero_axises(const bool* v[2]) {
-        if (!v[0]) this->x = 0;
-        if (!v[1]) this->y = 0;
+        if (!v[0]) this->data.x = 0;
+        if (!v[1]) this->data.y = 0;
     }
     inline void zero_axises(T v) {
-        if (v[0] <= 0) this->x = 0;
-        if (v[1] <= 0) this->y = 0;
+        if (v[0] <= 0) this->data.x = 0;
+        if (v[1] <= 0) this->data.y = 0;
     }
     inline void zero_axises(const int v[2]) {
-        if (!v[0]) this->x = 0;
-        if (!v[1]) this->y = 0;
+        if (!v[0]) this->data.x = 0;
+        if (!v[1]) this->data.y = 0;
     }
     inline void zero(void) {
-        this->x = 0;
-        this->y = 0;
+        this->data.x = 0;
+        this->data.y = 0;
     }
     inline T swizzled(int x, int y) {
         T t;
@@ -57,12 +58,15 @@ public:
         T t;
         return t;
     }
+    inline bool operator!=(const T &rhs) {return !this->operator==(rhs);}
 };
 
 
 template <class T>
-class mm_2: public _mm2<float, T> { // integer
+class mm_2: public _mm2<float, T> { // 32bit float
 public:
+    operator __m64();
+    operator const __m64();
     inline void fill(const float n);
     inline void swizzle(int d[2]);
     inline void swizzle(int x, int y);
@@ -73,7 +77,7 @@ public:
     inline T operator/=(const T &rhs);
     inline T operator^=(const T &rhs);
     inline bool operator==(const T &rhs);
-    inline bool operator!=(const T &rhs);
+    //inline bool operator!=(const T &rhs);
     inline void XX(void);
     inline void YY(void);
     inline void YX(void);
@@ -81,22 +85,70 @@ public:
 
 
 template <class T>
-class mmi_2: public _mm2<int, T> { // float
+class mmi_2: public _mm2<int, T> { // 32bit integer
 public:
-    void fill(const float n);
-    inline void swizzle(int d[2]);
-    inline void swizzle(int x, int y);
-    inline void reverse(void);
-    inline T operator+=(const T &rhs);
-    inline T operator-=(const T &rhs);
-    inline T operator*=(const T &rhs);
-    inline T operator/=(const T &rhs);
-    inline T operator^=(const T &rhs);
-    inline bool operator==(const T &rhs);
-    inline bool operator!=(const T &rhs);
-    inline void XX(void);
-    inline void YY(void);
-    inline void YX(void);
+    operator __m64();
+    operator const __m64();
+    void fill(const float n) {
+        __m64 m = _mm_set1_pi32(n);
+        this->data = m;
+    }
+    void zero(void) {
+        __m64 m = _mm_setzero_si64();
+        this->data = m;
+    }
+    void swizzle(int d[2]) {
+        this->data.x = (d[0] <= 0) ? this->data.x : this->data.y;
+        this->data.y = (d[1] <= 0) ? this->data.x : this->data.y;
+    }
+    void swizzle(int x, int y) {
+        this->data.x = (x <= 0) ? this->data.x : this->data.y;
+        this->data.y = (y <= 0) ? this->data.x : this->data.y;
+    }
+    inline void reverse(void) {
+        __m64 m = _mm_setr_pi32(this->data);
+        this->data = m;
+    }
+    inline T operator+=(const T &rhs) {
+        __m64 m = _mm_add_pi32(this->data, rhs.data);
+        this->data = m;
+        return *this;
+    }
+    inline T operator-=(const T &rhs) {
+        __m64 m = _mm_sub_pi32(this->data, rhs.data);
+        this->data = m;
+        return *this;
+    }
+    inline T operator*=(const T &rhs) {
+        this->data.x *= rhs.data.x;
+        this->data.y *= rhs.data.y;
+        return *this;
+    }
+    inline T operator/=(const T &rhs) {
+        this->data.x /= rhs.data.x;
+        this->data.y /= rhs.data.y;
+        return *this;
+    }
+    inline T operator^=(const T &rhs) {
+        __m64 m = _mm_xor_si64(this->data, rhs);
+        this->data = m;
+    }
+    inline bool operator==(const T &rhs) {
+        return this.data.x == rhs.data.x && this.data.y == rhs.data.y;
+    }
+    //inline bool operator!=(const T &rhs);
+    inline void XX(void) {
+        __m64 m = _mm_set_pi32(this->data.x);
+        this->data = m;
+    }
+    inline void YY(void) {
+        __m64 m = _mm_set_pi32(this->data.y);
+        this->data = m;
+    }
+    inline void YX(void) {
+        __m64 m = _mm_setr_pi32(this->data);
+        this->data = m;
+    }
 };
 
 
