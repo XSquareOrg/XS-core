01f10d5ba7a5831ea2fda5a4d6d52a325e2cf9da
Speks of Dust <speksofdust@users.noreply.github.com>
2017-02-27 20:51:45 -0500

    added set and get_col funcs, added typedefs for each type of mat3u & mat4u

--- src/mat/sse/mat3.hpp
+++ src/mat/sse/mat3.hpp
@@ -14,7 +14,6 @@
  *   not, see http://www.gnu.org/licenses/.                                 *
 * ------------------------------------------------------------------------- */
 
-
 #ifndef XS_CORE_MAT3_SSE__
 #define XS_CORE_MAT3_SSE__
 
@@ -28,7 +27,7 @@ namespace xs_core {
 template <class MM, typename NUM>
 class mat3u {
 public:
-    // data is stored as 3 4d arrays the last item is not used
+    // data is stored as 3 4d arrays (the last item is not used)
     union {NUM a[4]; MM v;} x; // a b c X
     union {NUM a[4]; MM v;} y; // d e f X
     union {NUM a[4]; MM v;} z; // g h i X
@@ -36,9 +35,52 @@ public:
     const unsigned height = 3;
 };
 
+typedef mat3u<__m128i, int> mat3ui;
+typedef mat3u<__m128, float> mat3uf;
+
+void _set_mat3i(mat3ui targ,
+        const int a, const int b, const int c,
+        const int d, const int e, const int f,
+        const int g, const int h, const int i) {
+    // Linear fill for 3x3 int
+    targ.x.v = _mm_set_epi32(a, b, c, 0);
+    targ.y.v = _mm_set_epi32(d, e, f, 0);
+    targ.z.v = _mm_set_epi32(g, h, i, 0);
+}
+void _set_mat3f(mat3uf targ,
+        const float a, const float b, const float c,
+        const float d, const float e, const float f,
+        const float g, const float h, const float i) {
+    // Linear fill for 3x3 float
+    targ.x.v = _mm_set_ps(a, b, c, 0);
+    targ.y.v = _mm_set_ps(d, e, f, 0);
+    targ.z.v = _mm_set_ps(g, h, i, 0);
+}
+__m128i _get_col_mat3i(const mat3ui targ, const unsigned i) {
+    // get column from a mat3ui as an __m128i
+    __m128i x = _mm_set_epi32(targ.x.a[i], targ.y.a[i], targ.z.a[i], 0);
+    return x;
+}
+__m128 _get_col_mat3f(const mat3uf targ, const unsigned i) {
+    // get column from a mat3uf as an __m128
+    __m128 x = _mm_set_ps(targ.x.a[i], targ.y.a[i], targ.z.a[i], 0);
+    return x;
+}
+void _set_col_mat3i(mat3uf targ, unsigned i, int x, int y, int z) {
+    targ.x.a[i] = x;
+    targ.y.a[i] = y;
+    targ.z.a[i] = z;
+}
+void _set_col_mat3f(mat3uf targ, unsigned i, float x, float y, float z) {
+    targ.x.a[i] = x;
+    targ.y.a[i] = y;
+    targ.z.a[i] = z;
+}
+
 
 template <class U, typename NUM, class T>
 class Mat3Base {
+protected:
     U u;
 public:
     const unsigned width(void) {return 3;}
@@ -67,20 +109,34 @@ public:
 };
 
 
-class Mat3i: public Mat3Base<mat3u<__m128i, int>, int, Mat3i> { // 3x3 int
+class Mat3i: public Mat3Base<mat3ui, int, Mat3i> { // 3x3 int
 public:
+    inline void set(const int a, const int b, const int c,
+            const int d, const int e, const int f,
+            const int g, const int h, const int i) {
+        _set_mat3i(this->u, a, b, c, d, e, f, g, h, i);
+    }
     inline Mat3i operator+=(const Mat3i& rhs);
     inline Mat3i operator-=(const Mat3i& rhs);
     inline Mat3i operator*=(const int rhs);
     inline Mat3i operator*=(const Mat3i& rhs);
+    inline bool operator==(const Mat3i& rhs);
+    inline bool operator!=(const Mat3i& rhs);
 };
 
-class Mat3f: public Mat3Base<mat3u<__m128, float>, int, Mat3i> { // 3x3 float
+class Mat3f: public Mat3Base<mat3uf, int, Mat3i> { // 3x3 float
 public:
+    inline void set(const float a, const float b, const float c,
+            const float d, const float e, const float f,
+            const float g, const float h, const float i) {
+        _set_mat3f(this->u, a, b, c, d, e, f, g, h, i);
+    }
     inline Mat3f operator+=(const Mat3f& rhs);
     inline Mat3f operator-=(const Mat3f& rhs);
     inline Mat3f operator*=(const float rhs);
     inline Mat3f operator*=(const Mat3f& rhs);
+    inline bool operator==(const Mat3f& rhs);
+    inline bool operator!=(const Mat3f& rhs);
 };
 
 
--- src/mat/sse/mat4.hpp
+++ src/mat/sse/mat4.hpp
@@ -14,7 +14,6 @@
  *   not, see http://www.gnu.org/licenses/.                                 *
 * ------------------------------------------------------------------------- */
 
-
 #ifndef XS_CORE_MAT4_SSE__
 #define XS_CORE_MAT4_SSE__
 
@@ -35,9 +34,56 @@ public:
     const unsigned height = 4;
 };
 
+typedef mat4u<__m128i, int> mat4ui;
+typedef mat4u<__m128, float> mat4uf;
+
+void _set_mat4i(mat4ui targ,
+        const int a, const int b, const int c, const int d,
+        const int e, const int f, const int g, const int h,
+        const int i, const int j, const int k, const int l,
+        const int m, const int n, const int o, const int p) {
+    targ.x.v = _mm_set_epi32(a, b, c, d);
+    targ.y.v = _mm_set_epi32(e, f, g, h);
+    targ.z.v = _mm_set_epi32(i, j, k, l);
+    targ.w.v = _mm_set_epi32(m, n, o, p);
+}
+void _set_mat4f(mat4uf targ,
+        const float a, const float b, const float c, const float d,
+        const float e, const float f, const float g, const float h,
+        const float i, const float j, const float k, const float l,
+        const float m, const float n, const float o, const float p) {
+    targ.x.v = _mm_set_ps(a, b, c, d);
+    targ.y.v = _mm_set_ps(e, f, g, h);
+    targ.z.v = _mm_set_ps(i, j, k, l);
+    targ.w.v = _mm_set_ps(m, n, o, p);
+}
+__m128i _get_col_mat3i(const mat4ui targ, const unsigned i) {
+    // get column from a mat3ui as an __m128i
+    __m128i x = _mm_set_epi32(targ.x.a[i], targ.y.a[i], targ.z.a[i], targ.w.a[i]);
+    return x;
+}
+__m128 _get_col_mat3f(const mat4uf targ, const unsigned i) {
+    // get column from a mat3uf as an __m128
+    __m128 x = _mm_set_ps(targ.x.a[i], targ.y.a[i], targ.z.a[i], targ.w.a[i]);
+    return x;
+}
+void _set_col_mat4i(mat4uf targ, unsigned i, int x, int y, int z, int w) {
+    targ.x.a[i] = x;
+    targ.y.a[i] = y;
+    targ.z.a[i] = z;
+    targ.w.a[i] = w;
+}
+void _set_col_mat4f(mat4uf targ, unsigned i, float x, float y, float z, float w) {
+    targ.x.a[i] = x;
+    targ.y.a[i] = y;
+    targ.z.a[i] = z;
+    targ.w.a[i] = w;
+}
+
 
 template <class U, typename NUM, class T>
 class Mat4Base {
+protected:
     U u;
 public:
     const unsigned width(void) {return 4;}
@@ -66,23 +112,38 @@ public:
 };
 
 
-class Mat4i: public Mat4Base<mat4u<__m128i, int>, int, Mat4i> { // 4x4 int
+class Mat4i: public Mat4Base<mat4ui, int, Mat4i> { // 4x4 int
 public:
+    inline void set(const int a, const int b, const int c, const int d,
+            const int e, const int f, const int g, const int h,
+            const int i, const int j, const int k, const int l,
+            const int m, const int n, const int o, const int p) {
+        _set_mat4i(this->u, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
+    }
     inline Mat4i operator+=(const Mat4i& rhs);
     inline Mat4i operator-=(const Mat4i& rhs);
     inline Mat4i operator*=(const int rhs);
     inline Mat4i operator*=(const Mat4i& rhs);
+    inline bool operator==(const Mat4i& rhs);
+    inline bool operator!=(const Mat4i& rhs);
 };
 
 
-class Mat4f: public Mat4Base<mat4u<__m128, float>, float, Mat4f> { // 4x4 float
+class Mat4f: public Mat4Base<mat4uf, float, Mat4f> { // 4x4 float
 public:
+    inline void set(const float a, const float b, const float c, const float d,
+            const float e, const float f, const float g, const float h,
+            const float i, const float j, const float k, const float l,
+            const float m, const float n, const float o, const float p) {
+        _set_mat4f(this->u, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
+    }
     inline Mat4i operator+=(const Mat4i& rhs);
     inline Mat4i operator-=(const Mat4i& rhs);
     inline Mat4i operator*=(const int rhs);
     inline Mat4i operator*=(const Mat4i& rhs);
+    inline bool operator==(const Mat4f& rhs);
+    inline bool operator!=(const Mat4f& rhs);
 };
-
 
 
 } // xs_core
